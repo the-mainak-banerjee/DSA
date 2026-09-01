a8bd88ce9babb16cdb6ffd312858f111bbc2959f
Mainak <developer.mainakbanerjee@gmail.com>
2026-09-01 12:28:24 +0530

    day-3: bubble sort

---
+++ searching&sorting/bubble-sort/app.js
@@ -0,0 +1,19 @@
+const arr = [5, 4,7,2, 9, 1, 0];
+
+function bubbleSort(arr) {
+  const n = arr.length;
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = 0; j < n - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+
+  return arr;
+}
+
+const result = bubbleSort(arr);
+console.log(result);
